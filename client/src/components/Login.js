4489b9c58e5f0e6473912c0925856a5831ababca
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-20 10:59:59 -0500

    added login form and logic.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,48 @@
 import React from "react";
 
-const Login = () => {
+import { useInput } from '../utils/useInput'
+import axiosWithAuth from "../utils/axiosWithAuth";
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials, handleChanges] = useInput({
+		username: '',
+		password: ''
+  })
+  const handleSubmit = e =>{
+    e.preventDefault()
+    axiosWithAuth()
+    .post('/login',credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload);
+      console.log(res)
+      props.history.push('/bubbles');
+    })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <label>UserName:</label>
+        <input 
+        name='username'
+        type='text'
+        onChange={handleChanges}
+        value={credentials.username}
+        />
+        <label>Password:</label>
+        <input 
+        name='password'
+        type='text'
+        onChange={handleChanges}
+        value={credentials.password}
+        />
+        <button>
+          Login
+        </button>
+      </form>
     </>
   );
 };
